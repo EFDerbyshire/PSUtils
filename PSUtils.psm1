e716a7dbd57d21cde01f0a71c4a4fdf343de39d8
Ellis F. Derbyshire <efderbyshire@proton.me>
2025-02-03 18:54:46 +0000

    Create module and manifest

---
+++ PSUtils.psm1
@@ -0,0 +1,23 @@
+# Import version number
+$manifest = Import-PowerShellDataFile "$PSScriptRoot\PSUtils.psd1"
+$version = $manifest.ModuleVersion
+
+# Change Powershell titlebar
+try 
+{
+    $host.UI.RawUI.WindowTitle="PSUtils $version"    
+}
+catch {}
+
+# Print module logo
+$logo = @"
+    ____                               __         ______  ____  _ __    
+   / __ \____ _      _____  __________/ /_  ___  / / / / / / /_(_) /____
+  / /_/ / __ \ | /| / / _ \/ ___/ ___/ __ \/ _ \/ / / / / / __/ / / ___/
+ / ____/ /_/ / |/ |/ /  __/ /  (__  ) / / /  __/ / / /_/ / /_/ / (__  ) 
+/_/    \____/|__/|__/\___/_/  /____/_/ /_/\___/_/_/\____/\__/_/_/____/  
+
+ v$version
+"@
+
+Write-Host $logo -ForeGroundColor Cyan
